6d281bab4610566d3de99e2c6d701342fa375af9
JuliusPro <max.red@gmx.com>
2016-03-08 10:59:33 -0300

    Update ej24.c

--- Practica-65535/ej24.c
+++ Practica-65535/ej24.c
@@ -3,15 +3,18 @@
 int main(){
   
   // creo que anda, pero no lo probé mucho...
-  // Emm si el comentario está hecho en una cadena 
-  // también lo borra (a pesar de que no debería), pero bueh
   
-  int p = 0, i, borrarEOL = 0, borrarPosta = 0;
+  int p = 0, i, borrarEOL = 0, borrarPosta = 0, comillas = 0;
   char s[1000005];
   
-  while( (s[p++] = getchar()) != EOF ); // Lee el archivo y lo guarda
+  while( (s[p++] = getchar()) != 'x' ); // Lee el archivo y lo guarda
                                         // en el array s de tamaño p.
-  for(i = 0; i < p; i++){
+  for(i = 0; i < p - 1; i++){
+    if(comillas == 1){
+      putchar(s[i]);
+      if(s[i] == '"') comillas = 0;
+      continue;
+    }
     if(borrarPosta == 1){
       if(i + 1 < p && s[i] == '*' && s[i+1] == '/'){
         borrarPosta = 0;
@@ -25,6 +28,10 @@ int main(){
         borrarEOL = 0;
       }
       continue;
+    }
+    if(s[i] == '"'){
+      comillas = 1 - comillas; // si comillas era 1 pasa a ser 0 y
+                               // viceversa...
     }
     if( i + 1 < p && s[i] == '/' && s[i+1] == '/' ){
       borrarEOL = 1;
